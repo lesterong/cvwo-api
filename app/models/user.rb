933374e183c78753142bedcc40e2fe73f3e2685d
Lester <lesterong776@gmail.com>
2022-01-11 00:53:51 +0800

    Add password complexity requirement

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,14 @@ class User < ApplicationRecord
          :jwt_authenticatable,
          :registerable,
          jwt_revocation_strategy: JwtDenylist
-  
+
   has_many :tasks, dependent: :destroy
+  
+  validate :password_complexity
+
+  def password_complexity
+    return if password.blank? || password =~ /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\w\W]{8,}$/
+    
+    errors.add :password, 'Complexity requirement not met.'
+  end
 end
